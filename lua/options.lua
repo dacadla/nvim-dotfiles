b69559b8b3ea1a08bb03d45663dcd2bfa30cd499
dacadla <71786326+dacadla@users.noreply.github.com>
2023-08-08 13:16:14 +0200

    Set basic options

---
+++ lua/options.lua
@@ -0,0 +1,21 @@
+-- set leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- set number
+vim.o.number = true
+-- vim.o.relativenumber = true
+
+vim.o.signcolumn = 'yes'
+
+-- set tab behavior
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+-- set updatetime
+vim.o.updatetime = 300
+
+-- set colors
+vim.o.termguicolors = true
+
+vim.o.mouse = "a"
